3ff944305a1d9fe1826e721d15afb5fad2eaf53c
Ismael <Ismael@Notebook>
2016-09-07 17:06:22 -0300

    agregados modelos de persona y usuario

---
+++ Modelo/Persona.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Persona
+    {
+        private int _rut;
+        private char _dv;
+        private string _nombres;
+        private string _apellidos;
+        private DateTime _fechaNac;
+        /*  prodriamos agregar la fecha desde que es cliente, para hacer algun descuento por antiguedad
+         *  private DateTime _fechaReg;
+         *  public DateTime FechaReg{
+         *      get { return _fechReg; }
+         *  }
+         * 
+         *
+         */
+        
+        public int Rut
+        {
+            //creo que el set deberia ser solo a nivel de clase
+            get
+            {
+                return _rut;
+            }
+            protected set
+            {
+                _rut = value;
+            }
+        }
+
+        public char Dv
+        {
+            get { return _dv; }
+            protected set { _dv = value; }
+        }
+
+         public string Nombre
+        {
+            get { return _nombres; }
+            set { _nombres = value; }
+        }
+
+        public string Apellidos
+        {
+            get { return _apellidos; }
+            set { _apellidos = value; }
+        }
+
+        public DateTime FechaNac
+        {
+            get { return _fechaNac; }
+            set { _fechaNac = value; }
+        }
+    }
+}
---
+++ Modelo/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Usuario
+    {
+        private Persona _persona;
+        private string _pwd;//deberiamos guardarla hasheada
+
+        public Persona Persona
+        {
+            get { return _persona; }
+            //misma intencion que con rut, deberia ser cambiada solo dentro de la clase
+            protected set { _persona = value; }
+        }
+
+        public string Pwd
+        {
+            get { return _pwd; }
+            //igual que arriba debiese ser cambiada solo dentro de la misma clase
+            protected set { _pwd = value; }
+        }
+    }
+}
