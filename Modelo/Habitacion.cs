951f388f40f3414288c9b9adbca823c191337826
Ismael <Ismael@DESKTOP-S63D1VI>
2016-09-08 16:59:10 -0300

    añadidos objetos de reserva y habitacion

---
+++ Modelo/Habitacion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Habitacion
+    {
+        private int _numero;
+        private TipoHabitacion _tipo;
+        private double _costoDiario;
+
+        public int Numero
+        {
+            get { return _numero; }
+        }
+
+        public TipoHabitacion Tipo
+        {
+            get { return _tipo; }
+            set { _tipo = value; }
+        }
+
+        public double CostoDiario
+        {
+            get { return _costoDiario; }
+            set { _costoDiario = value; }
+        }
+
+        public Habitacion()
+        {
+
+        }
+
+        public Habitacion(int numero,TipoHabitacion tipo,double costoDiario)
+        {
+            _numero = numero;
+            _tipo = tipo;
+            _costoDiario = costoDiario;
+        }
+    }
+    public enum TipoHabitacion
+    {
+        Single,Doble,Suite
+    }
+}
---
+++ Modelo/Reserva.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Reserva
+    {
+        private DateTime _fechaIngreso;
+        private DateTime _fechaRetiro;
+        private Habitacion _habitacion;
+        private double _costoReserva;
+
+        public DateTime FechaIngreso
+        {
+            get { return _fechaIngreso; }
+            set { _fechaIngreso = value; }
+        }
+
+        public DateTime FechaRetiro
+        {
+            get { return _fechaRetiro; }
+            set { _fechaRetiro = value; }
+        }
+
+        public Habitacion Habitacion
+        {
+            get { return _habitacion; }
+            set { _habitacion = value; }
+        }
+
+        public double CostoReserva
+        {
+            get { return _costoReserva; }
+        }
+
+        public Reserva(DateTime _fechaIngreso, DateTime _fechaRetiro,Habitacion _habitacion)
+        {
+            this._fechaIngreso = _fechaIngreso;
+            this._fechaRetiro = _fechaRetiro;
+            this._habitacion = _habitacion;
+            int dias = 0;
+            TimeSpan ts = FechaIngreso - FechaRetiro;
+            dias = ts.Days;
+            _costoReserva = dias * Habitacion.CostoDiario;
+        }
+    }
+}
