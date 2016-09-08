ceaa45ae2dd7e1c9b35a21c49b4493b48e7f3cef
Ismael <Ismael@Notebook>
2016-09-08 19:47:33 -0300

    agregada pagina registro y ajustada

---
+++ Hotelera/Registro.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Hotelera
+{
+    public partial class Registro : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+        ///*la idea es que se despliegue el calendario con el boton*/
+        //protected void btnMuestraCalendario_Click(object sender, ImageClickEventArgs e)
+        //{
+        //    if (calendario1.Visible)
+        //    {
+        //        calendario1.Visible = false;
+        //    }
+        //    else
+        //    {
+        //        calendario1.Visible = true;
+        //    }
+        //}  Elimino el boton para dejarlo visible siempre, dando la opcion de elejir mes y año en combobox
+
+        //protected void calendario1_SelectionChanged(object sender, EventArgs e)
+        //{
+        //    txtFec.Text = calendario1.SelectedDate.ToLongDateString();
+        //    calendario1.Visible = false;
+        //}
+
+        /*no cacho muy bien que deberia ir en este evento*/
+        protected void btnRegistra_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("Login.aspx");
+        }
+
+    }
+}
---
+++ Hotelera/Reservas.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/master.Master" AutoEventWireup="true" CodeBehind="Reservas.aspx.cs" Inherits="Hotelera.Reservas" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="centerpage" runat="server">
+    <
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="menubar" runat="server">
+</asp:Content>
