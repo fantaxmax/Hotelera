2e13335e586f3116acc9a2ec6c8ba73072322f86
Ismael <Ismael@DESKTOP-7DBPPVH>
2016-11-22 19:28:55 -0300

    Agregado proyecto WCF

---
+++ ServiciosWCF/HoteleraWCF.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Modelo;
+
+namespace ServiciosWCF
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface HoteleraWCF
+    {
+        [OperationContract]
+        Usuario Login(string user, string pass);
+    }
+}
---
+++ ServiciosWCF/Services.svc.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Modelo;
+
+namespace ServiciosWCF
+{
+    public class Services : HoteleraWCF
+    {
+        Usuario HoteleraWCF.Login(string user, string pass)
+        {
+            Usuario u = Conector.getUsuario(getRut(user));
+            if (u != null & pass == u.Pwd)
+                return u;
+            return null;
+        }
+
+        protected int getRut(string rut)
+        {
+            if (validaRut(rut))
+            {
+                rut = rut.ToUpper();
+                rut = rut.Replace(".", "");
+                rut = rut.Replace("-", "");
+                int rutAux = int.Parse(rut.Substring(0, rut.Length - 1));
+                return rutAux;
+            }
+            return 0;
+        }
+
+        protected bool validaRut(string rut) //codigo prestado desde http://www.qualityinfosolutions.com/validador-de-rut-chileno-en-c/
+        {
+            bool validacion = false;
+            try
+            {
+                rut = rut.ToUpper();
+                rut = rut.Replace(".", "");
+                rut = rut.Replace("-", "");
+                int rutAux = int.Parse(rut.Substring(0, rut.Length - 1));
+
+                char dv = char.Parse(rut.Substring(rut.Length - 1, 1));
+
+                int m = 0, s = 1;
+                for (; rutAux != 0; rutAux /= 10)
+                {
+                    s = (s + rutAux % 10 * (9 - m++ % 6)) % 11;
+                }
+                if (dv == (char)(s != 0 ? s + 47 : 75))
+                {
+                    validacion = true;
+                }
+            }
+            catch (Exception)
+            {
+            }
+            return validacion;
+        }
+    }
+}
